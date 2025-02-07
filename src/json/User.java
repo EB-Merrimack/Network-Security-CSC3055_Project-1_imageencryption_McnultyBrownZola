779d65f9fe3297ffc2f9435081b7079ff89a13de
EB-Merrimack <144274749+EB-Merrimack@users.noreply.github.com>
2025-02-07 12:05:57 -0500

    adding private key element

--- src/json/User.java
+++ src/json/User.java
@@ -1,5 +1,9 @@
 package json;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Base64;
+
 public class User {
     private String id;
     private String publicKey;
@@ -19,5 +23,16 @@ public class User {
 
     public void setPublicKey(String publicKey) {
         this.publicKey = publicKey;
+    }
+
+    // Method to save private key encoded in base64 format to a file
+    public void savePrivateKeyToFile(String privateKey, String filename) throws IOException {
+        // Encode the private key in base64
+        String privateKeyEncoded = Base64.getEncoder().encodeToString(privateKey.getBytes());
+        
+        // Write the encoded private key to a file
+        try (FileOutputStream fos = new FileOutputStream(filename)) {
+            fos.write(privateKeyEncoded.getBytes());
+        }
     }
 }
