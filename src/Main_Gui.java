b7b8dacd9f74b57966d6a5cd46b051e5884eff8d
gabyzola <zolag@merrimack.edu>
2025-02-06 20:15:18 -0500

    update to Main Gui Class

--- src/Main_Gui.java
+++ src/Main_Gui.java
@@ -4,10 +4,13 @@ import Gui.GUIBuilder;
 public class Main_Gui {
     public static void main(String[] args) {
         GUIBuilder gui = new GUIBuilder();
+
         // What to do when the window closes:
         gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // Size of the window, in pixels
+
+        // Size of the window
         gui.setSize(800, 600);
+
         // Make the window "visible"
         gui.setVisible(true);
     }
