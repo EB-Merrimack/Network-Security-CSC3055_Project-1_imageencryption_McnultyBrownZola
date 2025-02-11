900b241453e6a20c2f5b85a1abab7864ccd1edcd
gabyzola <zolag@merrimack.edu>
2025-02-11 17:27:20 -0500

    Update to user class with getters and setters

--- src/json/Users.java
+++ src/json/Users.java
@@ -6,20 +6,32 @@ import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Users {
-    private List<User> keys;
+    private List<User> users;
 
-    // Constructor to initialize the keys list
+    // Constructor to initialize the list
     public Users() {
-        this.keys = new ArrayList<>();
+        this.users = new ArrayList<>();
     }
 
     // Getter
-    public List<User> getKeys() {
-        return keys;
+    public List<User> getUsers() {
+        return users;
     }
 
     // Setter
-    public void setKeys(List<User> keys) {
-        this.keys = keys;
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    // add a user
+    public void addUser(User user) {
+        if (user != null) {
+            this.users.add(user);
+        }
+    }
+
+    // remove a user
+    public void removeUser(User user) {
+        this.users.remove(user);
     }
 }
